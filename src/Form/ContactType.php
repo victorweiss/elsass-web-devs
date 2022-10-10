f18b3d39cc26a427e3a44a8ab8a7ea8f3482a943
Trait2Jauge <jonathan.claude.@gmail.com>
2022-10-10 17:13:43 +0200

    style: creation of base template

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -19,48 +19,42 @@ class ContactType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 'attr' => [
-                    'class' => 'form-control',
                     'minLenght' => '2',
                     'maxLenght' => '50',
+                    'placeholder' => 'Entrez votre nom',
                 ],
                 'label' => 'Nom',
-                'label_attr' => [
-                    'class' => 'form-label mt-4'
-                ]
             ])
             ->add(
                 'email',
-                EmailType::class
-                // , [
-                //     'attr' => [
-                //         'class' => 'form-control',
-                //         'minLenght' => '2',
-                //         'maxLenght' => '180',
-                //     ],
-                //     'label' => 'Adresse email',
-                //     'label_attr' => [
-                //         'class' => 'form-label mt-4'
-                //     ],
-                //     // 'constraints' => [
-                //     //     new Assert\NotBlank(),
-                //     //     new Assert\Email(),
-                //     //     new Assert\Lenght(['min' => '2', 'max' => 180])
-                //     // ]
-                // ]
+                EmailType::class,
+                [
+                    'attr' => [
+                        'minLenght' => '2',
+                        'maxLenght' => '180',
+                        'placeholder' => 'Entrez votre adresse email'
+                    ],
+                    'label' => 'Adresse email',
+                    // 'constraints' => [
+                    //     new Assert\NotBlank(),
+                    //     new Assert\Email(),
+                    //     new Assert\Lenght(['min' => '2', 'max' => 180])
+                    // ]
+                ]
             )
             ->add('subject', TextType::class, [
                 'attr' => [
-                    'class' => 'form-control',
                     'minLenght' => '2',
                     'maxLenght' => '50',
+                    'placeholder' => 'Entrez le sujet de votre demande'
                 ],
                 'label' => 'Sujet',
-                'label_attr' => [
-                    'class' => 'form-label mt-4'
-                ]
             ])
             ->add('message', TextareaType::class, [
-                'attr' => ['rows' => 6],
+                'attr' => [
+                    'rows' => 6,
+                    'placeholder' => 'Entrez votre message'
+                ],
             ]);
     }
 
