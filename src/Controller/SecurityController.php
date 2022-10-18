be5ce1ba265915653d06a8e888fdab6e9c0bd29a
Trait2Jauge <jonathan.claude.@gmail.com>
2022-10-18 17:37:18 +0200

    fix name route login + logout et creation sidebar admin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,10 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/logout', name: 'logout', methods: ['GET'])]
+    #[Route('/deconnexion', name: 'deconnexion', methods: ['GET'])]
     public function logout()
     {
-        // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
