60e0bd5a09f435a6fa35315f5d3bc0937278b642
Trait2Jauge <jonathan.claude.@gmail.com>
2022-10-19 03:29:54 +0200

    crud contact ok - corrections a faire sur apparition navbar et form user

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,10 @@
 import "./scss/style.scss"
 // import "bootstrap/js/src/dropdown"
 import "bootstrap/js/src/collapse"
+
+$(function() {
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar, #content').toggleClass('active');
+    });
+  });
---
+++ src/Controller/AdminContactController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/_admin/contact')]
+class AdminContactController extends AbstractController
+{
+    #[Route('/', name: 'admin_contact_index', methods: ['GET'])]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('admin_contact/index.html.twig', [
+            'contacts' => $contactRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_contact_show', methods: ['GET'])]
+    public function show(Contact $contact): Response
+    {
+        return $this->render('admin_contact/show.html.twig', [
+            'contact' => $contact,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_contact_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Contact $contact, ContactRepository $contactRepository): Response
+    {
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->save($contact, true);
+
+            return $this->redirectToRoute('admin_contact_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_contact/edit.html.twig', [
+            'contact' => $contact,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_contact_delete', methods: ['POST'])]
+    public function delete(Request $request, Contact $contact, ContactRepository $contactRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
+            $contactRepository->remove($contact, true);
+        }
+
+        return $this->redirectToRoute('admin_contact_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
