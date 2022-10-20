539b6b4fb19bc331bedaaecc7bc871ab8ad271b1
Trait2Jauge <jonathan.claude.@gmail.com>
2022-10-20 15:08:06 +0200

    fix sidebar admin

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/connexion', name: 'connexion')]
+    #[Route('/connexion', name: 'login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/deconnexion', name: 'deconnexion', methods: ['GET'])]
+    #[Route('/deconnexion', name: 'logout', methods: ['GET'])]
     public function logout()
     {
     }
