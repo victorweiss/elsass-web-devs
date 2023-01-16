abe2beb23c27fe0ef3830329b3dc2391839cc4ee
Victor Weiss <bonjour@victor-weiss.fr>
2023-01-16 14:45:20 +0100

    review : Sitemap

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -4,20 +4,14 @@ namespace App\Controller;
 
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SitemapController extends AbstractController
 {
-
-    private $blogArticleRepository;
-
-    public function __construct(ArticleRepository $blogArticleRepository)
-    {
-        $this->blogArticleRepository = $blogArticleRepository;
-       
-    }
+    public function __construct(
+        private ArticleRepository $blogArticleRepository
+    ) {}
 
     #[Route('/sitemap.xml', name: 'sitemap')]
     public function index()
@@ -25,7 +19,7 @@ class SitemapController extends AbstractController
         // find published blog posts from db
         $articles = $this->blogArticleRepository->findBy(['marking' => 'Actif']);
         $urls = [];
-       
+
         foreach ($articles as $article) {
             $urls[] = [
                 'loc' => $this->generateUrl(
@@ -34,17 +28,10 @@ class SitemapController extends AbstractController
                     UrlGeneratorInterface::ABSOLUTE_URL
                 ),
                 'lastmod' => $article->getUpdatedAt()->format('Y-m-d'),
-                'changefreq' => 'daily',
-                'priority' => '0.5',
             ];
         }
-        
 
-
-        $response = new Response(
-            $this->renderView('./sitemap/sitemap.html.twig', ['urls' => $urls]),
-            200
-        );
+        $response = $this->render('sitemap/sitemap.html.twig', ['urls' => $urls]);
         $response->headers->set('Content-Type', 'text/xml');
 
         return $response;
