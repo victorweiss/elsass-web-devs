d50293611479ac2a599b9cbf53cf97a7ac676229
Victor Weiss <bonjour@victor-weiss.fr>
2023-01-16 16:16:53 +0100

    fix: ckeditor install

--- docker/php/scripts/prod/docker-start.sh
+++ docker/php/scripts/prod/docker-start.sh
@@ -10,7 +10,7 @@ composer dump-autoload --no-dev --classmap-authoritative
 
 php bin/console doctrine:migration:migrate -n
 
-php bin/console ckeditor:install
+php bin/console ckeditor:install --clear=skip
 php bin/console elfinder:install
 php bin/console assets:install --symlink public
 
