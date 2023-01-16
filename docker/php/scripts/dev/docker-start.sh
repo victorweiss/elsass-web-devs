3a78cd7135ecec63feffd9ef04f1e0d46796556f
Victor Weiss <bonjour@victor-weiss.fr>
2023-01-16 15:56:33 +0100

    up: docker-start.sh

--- docker/php/scripts/dev/docker-start.sh
+++ docker/php/scripts/dev/docker-start.sh
@@ -9,4 +9,8 @@ php bin/console cache:warmup
 
 php bin/console doctrine:migration:migrate -n
 
+php bin/console ckeditor:install
+php bin/console elfinder:install
+php bin/console assets:install
+
 php-fpm
