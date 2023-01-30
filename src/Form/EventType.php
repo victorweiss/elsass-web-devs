fb606d036ce977afc6a65725bab3a7bdcdb976e9
Trait2Jauge <jonathan.claude.93@gmail.com>
2023-01-30 15:48:25 +0100

    fix form event

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,6 +6,7 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,15 +20,18 @@ class EventType extends AbstractType
             ->add('title')
             ->add('body', CKEditorType::class)
             ->add('imageFile', VichImageType::class, [
-                'required' => false
+                'required' => true
             ])
             ->add('isBookingAvailable')
-            ->add('totalSeats')
+            ->add('totalSeats', NumberType::class)
             ->add('startAt', DateTimeType::class, [
                 'date_widget' => 'single_text',
                 'time_widget' => 'single_text'
                 ])
-            ->add('endAt', DateTimeType::class)
+            ->add('endAt', DateTimeType::class, [
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text'
+                ])
             ->add('adminNote', TextareaType::class, [
                 'attr' => [
                     'rows' => 6,
